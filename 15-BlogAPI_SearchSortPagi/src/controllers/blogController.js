d39b34190dae29dfe8a80ce5a0249408f5b3ee33
kplnhsyn <kplnhsyn.49@gmail.com>
2023-10-16 11:21:43 +0200

    req.getModelList and req.ModelListDetails

--- 15-BlogAPI_SearchSortPagi/src/controllers/blogController.js
+++ 15-BlogAPI_SearchSortPagi/src/controllers/blogController.js
@@ -1,89 +1,84 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     EXPRESSJS - BLOG Project with Mongoose
 ------------------------------------------------------- */
 // https://mongoosejs.com/docs/queries.html
 
 // Catch async-errors and send to errorHandler:
-require('express-async-errors')
+require("express-async-errors");
 
 /* ------------------------------------------------------- */
 
 // Call Models:
-const { BlogCategory, BlogPost } = require('../models/blogModel')
-
+const { BlogCategory, BlogPost } = require("../models/blogModel");
 
 // ------------------------------------------
 // BlogCategory
 // ------------------------------------------
 module.exports.BlogCategory = {
-
     list: async (req, res) => {
-
         // const data = await BlogCategory.find()
-        const data = await req.getModelList(BlogCategory)
+        const data = await req.getModelList(BlogCategory);
 
         res.status(200).send({
             error: false,
             count: data.length,
-            result: data
-        })
+            details: await req.getModelListDetails(BlogCategory),
+            result: data,
+        });
     },
 
     create: async (req, res) => {
-
-        const data = await BlogCategory.create(req.body)
+        const data = await BlogCategory.create(req.body);
 
         res.status(201).send({
             error: false,
             body: req.body,
             result: data,
-        })
+        });
     },
 
     read: async (req, res) => {
-
         // req.params.categoryId
         // const data = await BlogCategory.findById(req.params.categoryId)
-        const data = await BlogCategory.findOne({ _id: req.params.categoryId })
+        const data = await BlogCategory.findOne({ _id: req.params.categoryId });
 
         res.status(200).send({
             error: false,
-            result: data
-        })
-
+            result: data,
+        });
     },
 
     update: async (req, res) => {
-        
         // const data = await BlogCategory.findByIdAndUpdate(req.params.categoryId, req.body, { new: true }) // return new-data
-        const data = await BlogCategory.updateOne({ _id: req.params.categoryId }, req.body)
+        const data = await BlogCategory.updateOne(
+            { _id: req.params.categoryId },
+            req.body
+        );
 
         res.status(202).send({
             error: false,
             body: req.body,
             result: data, // update infos
-            newData: await BlogCategory.findOne({ _id: req.params.categoryId })
-        })
-
+            newData: await BlogCategory.findOne({ _id: req.params.categoryId }),
+        });
     },
 
     delete: async (req, res) => {
-        
-        const data = await BlogCategory.deleteOne({ _id: req.params.categoryId })
+        const data = await BlogCategory.deleteOne({
+            _id: req.params.categoryId,
+        });
 
-        res.sendStatus( (data.deletedCount >= 1) ? 204 : 404 )
-
+        res.sendStatus(data.deletedCount >= 1 ? 204 : 404);
     },
-}
-
+};
 
 // ------------------------------------------
 // BlogPost
 // ------------------------------------------
 module.exports.BlogPost = {
-
     list: async (req, res) => {
+        // biz bu islemleri buraya yazdigimiz icin sdc  BlogPost da yapiyoruz  BlogCategory ve user dada yapmak istedigimizden her defasinda yeniden yazmamak icin bir middleware olarak yazip istedigimiz her yerde cagirip kullanmak icin buraya degil middlewares/findSearchSortPage.js de yaziyorum ve index.js de require() yapip calistiriyoruz
 
         /*
         // Searching & Sorting & Pagination:
@@ -109,6 +104,7 @@ module.exports.BlogPost = {
 
         let page = Number(req.query?.page)
         page = (page > 0 ? page : 1) - 1 // Backend'de sayfaNo her zaman -1'dir.
+        not:.env dosyasindan veri ne yazarsak yazalim string gelir 
         // console.log('page', typeof page, page)
 
         let skip = Number(req.query?.skip) // İstenirse url'de ?skip=10 gibi değer gönderilebilir.
@@ -120,77 +116,76 @@ module.exports.BlogPost = {
         const data = await BlogPost.find(search).sort(sort).skip(skip).limit(limit).populate('blogCategoryId')
         */
 
-        const data = await req.getModelList(BlogPost, 'blogCategoryId')
+        const data = await req.getModelList(BlogPost, "blogCategoryId");
 
         res.status(200).send({
             error: false,
             count: data.length,
             details: await req.getModelListDetails(BlogPost),
             result: data,
-        })
+        });
     },
 
     listCategoryPosts: async (req, res) => {
-
-        const data = await BlogPost.find({ blogCategoryId: req.params.categoryId }).populate('blogCategoryId')
+        const data = await BlogPost.find({
+            blogCategoryId: req.params.categoryId,
+        }).populate("blogCategoryId");
 
         res.status(200).send({
             error: false,
             count: data.length,
-            result: data
-        })
+            result: data,
+        });
     },
 
     // CRUD ->
 
     create: async (req, res) => {
-        
         // const data = await BlogPost.create({
         //     fieldName: 'value',
         //     fieldName: 'value',
         //     fieldName: 'value',
         // })
-        const data = await BlogPost.create(req.body)
+        const data = await BlogPost.create(req.body);
 
         res.status(201).send({
             error: false,
             body: req.body,
             result: data,
-        })
+        });
     },
 
     read: async (req, res) => {
-
         // req.params.postId
         // const data = await BlogPost.findById(req.params.postId)
-        const data = await BlogPost.findOne({ _id: req.params.postId }).populate('blogCategoryId') // get Primary Data
+        const data = await BlogPost.findOne({
+            _id: req.params.postId,
+        }).populate("blogCategoryId"); // get Primary Data
 
         res.status(200).send({
             error: false,
-            result: data
-        })
-
+            result: data,
+        });
     },
 
     update: async (req, res) => {
-        
         // const data = await BlogPost.findByIdAndUpdate(req.params.postId, req.body, { new: true }) // return new-data
-        const data = await BlogPost.updateOne({ _id: req.params.postId }, req.body)
+        const data = await BlogPost.updateOne(
+            { _id: req.params.postId },
+            req.body
+        );
 
         res.status(202).send({
             error: false,
             body: req.body,
             result: data, // update infos
-            newData: await BlogPost.findOne({ _id: req.params.postId })
-        })
-
+            newData: await BlogPost.findOne({ _id: req.params.postId }),
+        });
     },
 
     delete: async (req, res) => {
-        
-        const data = await BlogPost.deleteOne({ _id: req.params.postId })
+        const data = await BlogPost.deleteOne({ _id: req.params.postId });
 
-        res.sendStatus( (data.deletedCount >= 1) ? 204 : 404 )
-
+        res.sendStatus(data.deletedCount >= 1 ? 204 : 404);
     },
-}
+};
